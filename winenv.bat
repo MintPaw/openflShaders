a85129c14c1265750ba543da64d6555a4588b8fa
MintPaw <jerusanders@gmail.com>
2015-07-18 17:14:54 -0600

    Fixed horrid errors in winenv

--- winenv.bat
+++ winenv.bat
@@ -1,13 +1,11 @@
-if "$1"=="flash" do (
+if "%1"=="flash" (
 	doskey b=del bin\flash\bin\openflShaders.swf ^& cls ^& openfl build flash -debug
 	doskey r=bin\flash\bin\openflShaders.swf
 	doskey bb=del bin\flash\bin\openflShaders.swf ^& cls ^& openfl build flash -debug ^& echo. ^& echo. ^& echo. ^& bin\flash\bin\openflShaders.swf
 )
 
-if "$1"=="c++" do (
+if "%1"=="c++" (
 	doskey b=del bin\windows\cpp\bin\openflShaders.exe ^& cls ^& openfl build windows -debug -Dlegacy
 	doskey r=bin\windows\cpp\bin\openflShaders.exe
 	doskey bb=del bin\windows\cpp\bin\openflShaders.exe ^& cls ^& openfl build windows -debug -Dlegacy ^& echo. ^& echo. ^& echo. ^& bin\windows\cpp\bin\openflShaders.exe
 )
-
-cls
