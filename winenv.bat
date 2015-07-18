5252c7e831fa9c184081aec2530f304f9f9c672a
MintPaw <jerusanders@gmail.com>
2015-07-18 16:50:36 -0600

    Added c++ option to winenv

--- winenv.bat
+++ winenv.bat
@@ -1,3 +1,13 @@
-doskey b=del bin\flash\bin\openflShaders.swf ^& cls ^& openfl build flash -debug
-doskey r=bin\flash\bin\openflShaders.swf
-doskey bb=del bin\flash\bin\openflShaders.swf ^& cls ^& openfl build flash -debug ^& echo. ^& echo. ^& echo. ^& bin\flash\bin\openflShaders.swf
+if "$1"=="flash" do (
+	doskey b=del bin\flash\bin\openflShaders.swf ^& cls ^& openfl build flash -debug
+	doskey r=bin\flash\bin\openflShaders.swf
+	doskey bb=del bin\flash\bin\openflShaders.swf ^& cls ^& openfl build flash -debug ^& echo. ^& echo. ^& echo. ^& bin\flash\bin\openflShaders.swf
+)
+
+if "$1"=="c++" do (
+	doskey b=del bin\windows\cpp\bin\openflShaders.exe ^& cls ^& openfl build windows -debug
+	doskey r=bin\windows\cpp\bin\openflShaders.exe
+	doskey bb=del bin\windows\cpp\bin\openflShaders.exe ^& cls ^& openfl build windows -debug ^& echo. ^& echo. ^& echo. ^& bin\windows\cpp\bin\openflShaders.exe
+)
+
+cls
